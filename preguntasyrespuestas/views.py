3b47b8fd96679e20c215a6ff7f1829ea5b2394c9
ChrisTacam <chris_5679@hotmail.com>
2016-05-25 14:36:18 -0600

    Cambio titulo
    agregue caracteres a titulo

--- preguntasyrespuestas/views.py
+++ preguntasyrespuestas/views.py
@@ -3,8 +3,8 @@ from preguntasyrespuestas.models import Pregunta
 
 def index(request):
     preguntas = Pregunta.objects.all()
-    respuesta_string = "<h1>Preguntas <br/></h1>"
-    respuesta_string += '<br/>'.join(["id:%s, asunto:%s"%
+    respuesta_string = "<h1>**Preguntas** <br/></h1>"
+    respuesta_string += '<br/>'.join(["id:%s, asunto:%s "%
     (p.id,p.asunto) for p in preguntas])
     return HttpResponse(respuesta_string)
 
